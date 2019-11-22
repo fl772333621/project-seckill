92e5efa9d1f4b4b8d9afd3a5e4d0921b204fd7ee
mengwei <772333621@qq.com>
2019-11-22 16:47:04 +0800

    init project

---
+++ src/main/java/com/mfanw/seckill/ProjectSeckillApplication.java
@@ -0,0 +1,13 @@
+package com.mfanw.seckill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSeckillApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectSeckillApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mfanw/seckill/ProjectSeckillApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mfanw.seckill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectSeckillApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
