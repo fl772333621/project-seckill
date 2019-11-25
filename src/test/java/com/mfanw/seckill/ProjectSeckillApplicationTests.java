9baab55cfd6f879ae1726fdbafeeb47139c92d1a
mengwei <772333621@qq.com>
2019-11-25 09:47:47 +0800

    redis支持：支持操作long数值  @Resource RedisTemplate<String, Object> redisTemplate;

--- src/test/java/com/mfanw/seckill/ProjectSeckillApplicationTests.java
+++ src/test/java/com/mfanw/seckill/ProjectSeckillApplicationTests.java
@@ -3,11 +3,17 @@ package com.mfanw.seckill;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 
+import javax.annotation.Resource;
+
 @SpringBootTest
 class ProjectSeckillApplicationTests {
+
+    @Resource
+    private RedisTemplate<String, Object> redisTemplate;
 
     /**
      * 注入响应式的ReactiveRedisTemplate
@@ -16,12 +22,16 @@ class ProjectSeckillApplicationTests {
     private StringRedisTemplate stringRedisTemplate;
 
     @Test
-    void contextLoads() {
+    void testRedis() {
         ValueOperations<String, String> opsForValue = stringRedisTemplate.opsForValue();
         opsForValue.set("my_key", "my_value");
         String mono2 = opsForValue.get("my_key");
         System.out.println(mono2);
         System.out.println("==============");
+        for (int i = 0; i < 10; i++) {
+            Long result = redisTemplate.opsForValue().increment("time");
+            System.out.println(i + "\t" + result);
+        }
     }
 
 }
