793bce6a49c619507a7bb7cb259b25795001f105
mengwei <772333621@qq.com>
2019-11-25 09:33:03 +0800

    redis支持：完成redis 测试类

--- src/test/java/com/mfanw/seckill/ProjectSeckillApplicationTests.java
+++ src/test/java/com/mfanw/seckill/ProjectSeckillApplicationTests.java
@@ -1,13 +1,27 @@
 package com.mfanw.seckill;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 
 @SpringBootTest
 class ProjectSeckillApplicationTests {
 
+    /**
+     * 注入响应式的ReactiveRedisTemplate
+     */
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
     @Test
     void contextLoads() {
+        ValueOperations<String, String> opsForValue = stringRedisTemplate.opsForValue();
+        opsForValue.set("my_key", "my_value");
+        String mono2 = opsForValue.get("my_key");
+        System.out.println(mono2);
+        System.out.println("==============");
     }
 
 }
